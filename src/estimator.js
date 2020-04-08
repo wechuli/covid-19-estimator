6b733d63e0df700642c5b69bbc09048b8c8918aa
Wechuli <wechulipaul@yahoo.com>
2020-04-08 10:11:27 +0300

    destructor original data for easier manipulation

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,17 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const {
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  } = data;
+  return {
+    data,
+    impact: {},
+    severeImpact: {}
+  };
+};
 
 export default covid19ImpactEstimator;
