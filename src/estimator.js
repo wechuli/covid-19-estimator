673322b1f53ef32745d69f96f1e633914c65d5fc
Wechuli <wechulipaul@yahoo.com>
2020-04-09 13:03:59 +0300

    eslint

--- src/estimator.js
+++ src/estimator.js
@@ -1,12 +1,10 @@
 const { impact, severeImpact } = require('./constants');
 const { aggregateResults } = require('./aggregators');
 
-const covid19ImpactEstimator = (data) => {
-  return {
-    data,
-    impact: aggregateResults(data, impact),
-    severeImpact: aggregateResults(data, severeImpact)
-  };
-};
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: aggregateResults(data, impact),
+  severeImpact: aggregateResults(data, severeImpact)
+});
 
 module.exports = covid19ImpactEstimator;
